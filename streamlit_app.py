412744ea3c80b53a1ff88223c64ea82071567777
axelag2 <124352471+axelag2@users.noreply.github.com>
2026-02-04 16:45:31 +0100

    Restore Snowflake session and update success message
    Re-enable Snowflake session connection and adjust order submission success message.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,8 @@
 # Import python packages
 import streamlit as st
-# from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
-cnx = st.connection("snowflake")
-session = cnx.session()
 
 # Write directly to the app
 st.title(f"Custom Your Smoothie :cup_with_straw: !")
@@ -16,6 +14,8 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
