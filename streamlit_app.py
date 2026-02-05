c6e18e5d610e4227c73f5d812e98e5da78a1a55b
axelag2 <124352471+axelag2@users.noreply.github.com>
2026-02-05 12:00:55 +0100

    Fix method name from to_panda to to_pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit
 # st.stop()
 
 # convert the snowpark dataframe to panda dataframe so we can use the loc function
-pd_df=my_dataframe.to_panda()
+pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop()
 
